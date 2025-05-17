0dd9f73001723548377e94d90f43efd347db6476
candelanati <candelanati23@gmail.com>
2025-05-17 20:15:05 -0300

    products router base

--- src/routers/api/products.router.js
+++ src/routers/api/products.router.js
@@ -1,3 +1,47 @@
-import { Router } from "express";
+import { response, Router } from "express";
+
 const productsRouter = Router()
+
+const createOne = (req,res,next)=>{
+    try {
+        res.status(200).json({response:true})
+    } catch (error) {
+        next(error)//lleva al errorHandler
+    }
+}
+const readAll = (req,res,next)=>{
+    try {
+        res.status(200).json({response:true})
+    } catch (error) {
+        next(error)//lleva al errorHandler
+    }
+}
+const readById = (req,res,next)=>{
+    try {
+        res.status(200).json({response:true})
+    } catch (error) {
+        next(error)//lleva al errorHandler
+    }
+}
+const updateById = (req,res,next)=>{
+    try {
+        res.status(200).json({response:true})
+    } catch (error) {
+        next(error)//lleva al errorHandler
+    }
+}
+const destroyById = (req,res,next)=>{
+    try {
+        res.status(200).json({response:true})
+    } catch (error) {
+        next(error)//lleva al errorHandler
+    }
+}
+
+productsRouter.post("/",createOne)
+productsRouter.get("/",readAll)
+productsRouter.get("/:id",readById)
+productsRouter.put("/:id",updateById)
+productsRouter.delete("/:id",destroyById)
+
 export default productsRouter

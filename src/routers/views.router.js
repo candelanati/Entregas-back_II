fd79deb3678ba9250352f07443f7674e87363208
candelanati <candelanati23@gmail.com>
2025-05-18 18:27:26 -0300

    vistas - index.handlebars

--- src/routers/views.router.js
+++ src/routers/views.router.js
@@ -1,3 +1,17 @@
 import { Router } from "express";
+import { productsManager } from "../data/managers/mongo/manager.mongo.js";
 const ViewsRouter = Router()
+
+const indexView = async(req,res)=>{
+    try {
+        const products = await productsManager.readAll()
+        
+        res.status(200).render("index",{products})
+    } catch (error) {
+        res.status(error.statusCode||500).render("error",{error})
+    }
+}
+
+ViewsRouter.get("/",indexView)
+
 export default ViewsRouter

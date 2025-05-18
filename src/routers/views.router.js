a74abac81726ca1d96e8c24feb507266ec6b945a
candelanati <candelanati23@gmail.com>
2025-05-18 19:40:12 -0300

    actualizadas vistas index y detallada para agregar producto al carrito x default

--- src/routers/views.router.js
+++ src/routers/views.router.js
@@ -5,16 +5,19 @@ const ViewsRouter = Router()
 const indexView = async(req,res)=>{
     try {
         const products = await productsManager.readAll()
-        
-        res.status(200).render("index",{products})
+        // También pasamos el carrito por defecto (su ID) para usarlo en botones de "Agregar al carrito"
+        const defaultCartId = "682a32031b48dcb6e96c3014";
+        res.status(200).render("index",{products,defaultCartId})
     } catch (error) {
         res.status(error.statusCode||500).render("error",{error})
     }
 }
 const detailsView = async (req, res) => {
-  const { pid } = req.params
-  const product = await productsManager.readById(pid)
-  res.status(200).render("details", { product })
+    const { pid } = req.params
+    const product = await productsManager.readById(pid)
+    // También pasamos el carrito por defecto (su ID) para usarlo en botones de "Agregar al carrito"
+    const defaultCartId = "682a32031b48dcb6e96c3014";
+    res.status(200).render("details", { product, defaultCartId })
 }
 ViewsRouter.get("/",indexView)
 ViewsRouter.get("/details/:pid", detailsView)

a019c840cf66dd8835ff651c1d6eeb64c9bfe251
candelanati <candelanati23@gmail.com>
2025-06-20 22:29:34 -0300

    cookies router, controller

--- src/routers/api.router.js
+++ src/routers/api.router.js
@@ -4,13 +4,32 @@ import cartsRouter from "./api/carts.router.js";
 import usersRouter from "./api/users.router.js";
 import authRouter from "./api/auth.router.js";
 import sessionsRouter from "./api/sessions.router.js";
+import RouterHelper from "../helpers/router.helper.js";
+import cookiesRouter from "./api/cookies.router.js";
 
-const apiRouter = Router()
+// const apiRouter = Router()
+class ApiRouter extends RouterHelper {
+  constructor() {
+    super();
+    this.init();
+  }
+  init = () => {
+    this.use("/products", productsRouter);
+    this.use("/carts", cartsRouter);
+    this.use("/users", usersRouter);
+    this.use("/cookies", cookiesRouter);
+    this.use("/sessions", sessionsRouter);
+    this.use("/auth", authRouter);
+  };
+}
 
-apiRouter.use("/products",productsRouter)
-apiRouter.use("/carts",cartsRouter)
-apiRouter.use("/users",usersRouter)
-apiRouter.use("/auth",authRouter)
-apiRouter.use("/sessions",sessionsRouter)
+// apiRouter.use("/products",productsRouter)
+// apiRouter.use("/carts",cartsRouter)
+// apiRouter.use("/users",usersRouter)
+// apiRouter.use("/auth",authRouter)
+// apiRouter.use("/sessions",sessionsRouter)
 
+// export default apiRouter
+
+const apiRouter = new ApiRouter().getRouter();
 export default apiRouter
